6f67d479a08999cc6427658c64591bf639e67689
Willem Olding <willemolding@gmail.com>
2018-04-30 14:02:14 +1000

    moves setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import setup
 
+install_requires = [
+	'sklearn',
+	'numpy',
+	'scipy'
+	]
+
+
 setup(name='jointcd',
-      version='0.1',
+      version='1.0',
       description='Python package implementing change detection and change point estimation using a joint distribution estimated from a training set of similar signals',
       url='https://github.com/willemolding/JointGaussianChangeDetector',
+      install_requires=install_requires,
       author='Willem Olding',
       author_email='willemolding@gmail.com',
       license='MIT',
