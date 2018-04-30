48c91d8d74f04c3bb356714929b9f66f502c94f9
Willem Olding <willemolding@gmail.com>
2018-04-30 12:27:54 +1000

    adds setup.py and empty tests

---
+++ jointcd/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='jointcd',
+      version='0.1',
+      description='Python package implementing change detection and change point estimation using a joint distribution estimated from a training set of similar signals',
+      url='https://github.com/willemolding/JointGaussianChangeDetector',
+      author='Willem Olding',
+      author_email='willemolding@gmail.com',
+      license='MIT',
+      packages=['jointcd']
+      )
